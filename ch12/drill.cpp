0975334e61368b9ca53d7582a9b2a1f39150ebac
MrSolymi <solymosiati001220@gmail.com>
2022-02-19 15:37:47 +0100

    commit

---
+++ ch12/drill.cpp
@@ -0,0 +1,102 @@
+#include "../forras/Simple_window.h"
+#include "../forras/Graph.h"
+
+int main()
+{
+    int xmax = 600;
+    int ymax = 400;
+
+    Simple_window win {Point{200,200}, xmax, ymax, "My window"};
+    win.wait_for_button();
+    //Axis (tengely)
+    Axis xa {Axis::x, Point{20,300}, 280, 10, "x axis"};
+    win.attach(xa);
+    win.set_label("Cancas #2");
+    win.wait_for_button();
+
+    Axis ya {Axis::y, Point{20,300}, 280, 10, "y axis"};
+    ya.set_color(Color::cyan);
+    ya.label.set_color(Color::dark_red);
+    win.attach(ya);
+    win.set_label("Canvas #3");
+    win.wait_for_button();
+
+    Function sine {sin,0,100,Point{20,150},1000,50,50}; // sine curve
+    // plot sin() in the range [0:100) with (0,0) at (20,150)
+    // using 1000 points; scale x values *50, scale y values *50
+    win.attach(sine);
+    win.set_label("Canvas #4");
+    win.wait_for_button();
+    //Graphing a function (függvényábrázolás)
+    sine.set_color(Color::blue); // we changed our mind about sine’s color
+    Polygon poly; // a polygon; a Polygon is a kind of Shape
+    poly.add(Point{300,200}); // three points make a triangle
+    poly.add(Point{350,100});
+    poly.add(Point{400,200});
+    poly.set_color(Color::red);
+    poly.set_style(Line_style::dash);
+    win.attach(poly);
+    win.set_label("Canvas #5");
+    win.wait_for_button();
+    //Polygons
+    Rectangle r {Point{200,200}, 100, 50}; // top left corner, width, height
+    win.attach(r);
+    win.set_label("Canvas #6");
+    win.wait_for_button();
+    //Rectangles
+    Closed_polyline poly_rect;
+    poly_rect.add(Point{100,50});
+    poly_rect.add(Point{200,50});
+    poly_rect.add(Point{200,100});
+    poly_rect.add(Point{100,100});
+    poly_rect.add(Point{50,75});
+    win.attach(poly_rect);
+    win.wait_for_button();
+    //Fill
+    r.set_fill_color(Color::yellow); // color the inside of the rectangle
+    poly.set_style(Line_style(Line_style::dash,4));
+    poly_rect.set_style(Line_style(Line_style::dash,2));
+    poly_rect.set_fill_color(Color::green);
+    win.set_label("Canvas #7");
+    win.wait_for_button();
+    //Text
+    Text t {Point{150,150}, "Hello, graphical world!"};
+    win.attach(t);
+    win.set_label("Canvas #8");
+    win.wait_for_button();
+    //Text decor
+    t.set_font(Font::times_bold);
+    t.set_font_size(20);
+    win.set_label("Canvas #9");
+    win.wait_for_button();
+    //Images
+    Image ii {Point{100,50},"ch12/image1.jpeg"}; //400*200 jepg (can be jpg too)
+    win.attach(ii);
+    win.set_label("Canvas #10");
+    win.wait_for_button();
+    //Move images
+    ii.move(100,200);
+    win.set_label("Canvas #11");
+    win.wait_for_button();
+    //And much more
+    Point center = Point{100,200};
+    Circle c {center,50};
+    win.attach(c);
+    Ellipse e {center, 75,25};
+    e.set_color(Color::dark_red);
+    win.attach(e);
+    Mark m {center,'o'};
+    win.attach(m);
+
+    ostringstream oss;
+    oss << "screen size: " << x_max() << "*" << y_max() << "; window size: " << win.x_max() << "*" << win.y_max();
+    Text sizes {Point{100,20},oss.str()};
+    win.attach(sizes);
+
+    Image cal {Point{225,225},"ch12/200x200.gif"}; //200x200 gif
+    cal.set_mask(Point{40,40},200,150); // display center part of image
+    win.attach(cal);
+    
+    win.set_label("Canvas #12");
+    win.wait_for_button();
+}
---
+++ ch13/drill.cpp
@@ -0,0 +1,50 @@
+#include "../forras/Graph.h"
+#include "../forras/Simple_window.h"
+
+int main()
+{
+    using namespace Graph_lib;
+    int xmax = 800;
+    int ymax = 1000;
+
+    Simple_window win {Point{100,100}, xmax, ymax, "Canvas"};
+
+    int xgrid = 100;
+    int ygrid = 100;
+
+    Lines grid;
+
+    //2.
+    for (int x = xgrid; x <= 800; x += xgrid)
+    {
+        grid.add(Point{x,0}, Point{x, 800});
+    }
+    for (int y = ygrid; y <= 800; y += ygrid)
+    {
+        grid.add(Point{0,y}, Point{xmax, y});
+    }
+
+
+    //3.
+    Vector_ref<Rectangle> vr;
+
+    for (int i = 0; i < 8; i++)
+    {
+        vr.push_back(new Rectangle{Point{i*100,i*100}, Point{(i+1)*100,(i+1)*100}});
+        vr[vr.size()-1].set_fill_color(Color::red);
+        vr[vr.size()-1].set_color(Color::invisible);
+        win.attach(vr[vr.size()-1]);
+    }
+
+    win.attach(grid);
+    //4.
+    Image kutyus {Point{300,0},"ch13/200x200kep.jpeg"};
+    win.attach(kutyus);
+    Image kutyus1 {Point{600,200},"ch13/200x200kep.jpeg"};
+    win.attach(kutyus1);
+    Image kutyus2 {Point{0,400},"ch13/200x200kep.jpeg"};
+    win.attach(kutyus2);
+    
+
+    win.wait_for_button();
+}

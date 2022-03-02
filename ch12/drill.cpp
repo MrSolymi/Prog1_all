bcab2eecc120cc8728e089948ff3a5052d46f257
MrSolymi <solymosiati001220@gmail.com>
2022-03-02 10:25:41 +0100

    véglegesítés

--- ch12/drill.cpp
+++ ch12/drill.cpp
@@ -12,13 +12,14 @@ int main()
     Simple_window win {Point{200,200}, xmax, ymax, "My window"};
     win.wait_for_button();
     //Axis (tengely)
-    Axis xa {Axis::x, Point{20,300}, 280, 10, "x axis"};
+    Axis xa {Axis::x, Point{20,300}, 280, 20, "x axis"};
+    xa.set_color(Color::dark_blue);
     win.attach(xa);
     win.set_label("Cancas #2");
     win.wait_for_button();
 
-    Axis ya {Axis::y, Point{20,300}, 280, 10, "y axis"};
-    ya.set_color(Color::cyan);
+    Axis ya {Axis::y, Point{20,300}, 280, 20, "y axis"};
+    ya.set_color(Color::dark_red);
     ya.label.set_color(Color::dark_red);
     win.attach(ya);
     win.set_label("Canvas #3");
@@ -27,22 +28,23 @@ int main()
     Function sine {sin,0,100,Point{20,150},1000,50,50}; // sine curve
     // plot sin() in the range [0:100) with (0,0) at (20,150)
     // using 1000 points; scale x values *50, scale y values *50
+    sine.set_color(Color::dark_magenta);
     win.attach(sine);
     win.set_label("Canvas #4");
     win.wait_for_button();
     //Graphing a function (függvényábrázolás)
     sine.set_color(Color::blue); // we changed our mind about sine’s color
     Polygon poly; // a polygon; a Polygon is a kind of Shape
-    poly.add(Point{300,200}); // three points make a triangle
-    poly.add(Point{350,100});
-    poly.add(Point{400,200});
+    poly.add(Point{300, 200}); // three points make a triangle
+    poly.add(Point{350, 100});
+    poly.add(Point{400, 200});
     poly.set_color(Color::red);
-    poly.set_style(Line_style::dash);
+    poly.set_style(Line_style::dot);
     win.attach(poly);
     win.set_label("Canvas #5");
     win.wait_for_button();
     //Polygons
-    Rectangle r {Point{200,200}, 100, 50}; // top left corner, width, height
+    Rectangle r {Point{200,200}, 120, 50}; // top left corner, width, height
     win.attach(r);
     win.set_label("Canvas #6");
     win.wait_for_button();
@@ -53,13 +55,14 @@ int main()
     poly_rect.add(Point{200,100});
     poly_rect.add(Point{100,100});
     poly_rect.add(Point{50,75});
+    poly_rect.add(Point{150,130});
     win.attach(poly_rect);
     win.wait_for_button();
     //Fill
     r.set_fill_color(Color::yellow); // color the inside of the rectangle
     poly.set_style(Line_style(Line_style::dash,4));
     poly_rect.set_style(Line_style(Line_style::dash,2));
-    poly_rect.set_fill_color(Color::green);
+    poly_rect.set_fill_color(Color::yellow);
     win.set_label("Canvas #7");
     win.wait_for_button();
     //Text
@@ -69,8 +72,8 @@ int main()
     win.set_label("Canvas #8");
     win.wait_for_button();
     //Text decor
-    t.set_font(Font::times_bold);
-    t.set_font_size(20);
+    t.set_font(Font::courier_bold);
+    t.set_font_size(25);
     win.set_label("Canvas #9");
     win.wait_for_button();
     //Images
@@ -87,7 +90,7 @@ int main()
     Circle c {center,50};
     win.attach(c);
     Ellipse e {center, 75,25};
-    e.set_color(Color::dark_red);
+    e.set_color(Color::red);
     win.attach(e);
     Mark m {center,'o'};
     win.attach(m);

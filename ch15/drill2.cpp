900c7cb63b54b59d9929f37b93341db987d88c04
MrSolymi <solymosiati001220@gmail.com>
2022-03-17 19:05:31 +0100

    ch15 kész

--- ch15/drill2.cpp
+++ ch15/drill2.cpp
@@ -2,45 +2,114 @@
 
 struct Person
 {
-    string name;
-    int age;
+    private:
+        string firstName;
+        string secondName;
+        int age;
 
+    public:
+        void SetFirstName(string n)
+        {
+            firstName = n;
+
+            string invalid[] = {";", ":", "\"", "\'", "[", "]", "*", "&", "^", "%", "$", "#", "@", "!", "\""};
+
+            for (int i = 0; i < size(invalid); i++)
+            {
+                if (n.find(invalid[i]) != string::npos)
+                {
+                    throw runtime_error("Invalic character used in the name.");
+                }
+            }
+        }
+        string GetFirstName()
+        {
+            return firstName;
+        }
+        void SetSecondName(string n)
+        {
+            secondName = n;
+
+            string invalid[] = {";", ":", "\"", "\'", "[", "]", "*", "&", "^", "%", "$", "#", "@", "!", "\""};
+
+            for (int i = 0; i < size(invalid); i++)
+            {
+                if (n.find(invalid[i]) != string::npos)
+                {
+                    throw runtime_error("Invalic character used in the name.");
+                }
+            }
+        }
+        string GetSecondName()
+        {
+            return secondName;
+        }
+        void SetAge(int a)
+        {
+            age = a;
+            if(age < 0 || age >= 150)
+            {
+                throw runtime_error("The specified number is out of range.");
+            }
+        }
+        int GetAge()
+        {
+            return age;
+        }
+
+    /*
     //default constructor
     Person(){};
-
+    //constructor
     Person(string n, int a)
     {
         name = n;
         age = a;
-    }
-    
+    }*/
 };
-
 istream& operator>>(istream& is, Person& d){
     int age;
-    string name;
-    is >> name >> age;
+    string firstName;
+    string secondName;
+    is >> firstName >> secondName >> age;
     if(!is)
     {
         return is;
     }
-    Person person(name, age);
+    Person person;
+    person.SetFirstName(firstName);
+    person.SetSecondName(secondName);
+    person.SetAge(age);
     d = person;
     return is;
 }
-ostream& operator<<(ostream& os, const Person& d){
-    return os << d.name << " " << d.age << endl;
+ostream& operator<<(ostream& os, Person& d){
+    return os << d.GetFirstName() << " " << d.GetSecondName() << " " << d.GetAge() << endl;
 }
+
+vector<Person> persons;
 
 int main(){
     //1.
     Person firstPerson;
-    firstPerson.name = "Goofy";
-    firstPerson.age = 63;
-    cout << firstPerson.name << " is " << firstPerson.age << " years old." << endl;
+    firstPerson.SetFirstName("Goofy");
+    firstPerson.SetAge(63);
+    cout << firstPerson.GetFirstName() << " is " << firstPerson.GetAge() << " years old." << endl;
     //2.
     Person opPerson;
     cin >> opPerson;
     cout << opPerson;
-    //3.
+    //8.
+    for (int i = 0; i < 5; i++)
+    {
+        Person p;
+        cin >> p;
+        persons.push_back(p);
+    }
+    for (Person i : persons)
+    {
+        cout << i;
+    }
+
+    return 0;
 }
